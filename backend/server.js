6975f19fc7937252d189a63165b025d619ae83bd
ankita kundliya <ankita2kundliya@gmail.com>
2025-03-16 11:15:56 +0530

    finall

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // MongoDB Connection
-mongoose.connect("your-mongodb-uri", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
